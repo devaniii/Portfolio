0b84fa266affd23b8e23e4ea092aca640088061f
devaniii <devani.clx@gmail.com>
2024-05-03 00:26:54 -0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+/*const mongoose = require('mongoose');
+
+// Configuración de conexión a la base de datos MongoDB
+const mongoURI = 'mongodb://localhost:27017/BDPortfolio';
+
+module.exports = {
+    mongoURI: mongoURI,
+    connectToDB: async () => {
+        try {
+            await mongoose.connect(mongoURI);
+            console.log("Successful connection to MongoDB");
+        } catch (error) {
+            console.error("Error connecting to MongoDB:", error);
+            process.exit(1); // Exit the process with a failure code
+        }
+    }
+};*/
